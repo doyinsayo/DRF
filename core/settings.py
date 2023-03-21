4f57eea1a4b210d561b4f4474e4297ada040398b
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2023-03-21 19:10:30 +0000

    tests

--- core/settings.py
+++ core/settings.py
@@ -123,4 +123,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
