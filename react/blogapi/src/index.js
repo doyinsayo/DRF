6b5b025f96047ea0724817f32cafefe7cc3f435b
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2023-03-24 00:08:31 +0000

    .

--- react/blogapi/src/index.js
+++ react/blogapi/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-//import { ReactDOM } from "react-dom/client";
-//import * as ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client'
-//import * as serviceWorker from './serviceWorker';
 import './index.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -17,6 +14,7 @@ root.render(
 			<Routes>
 				<Route exact path="/" element={<App/>} />
 			</Routes>
+			<Footer/>
 		</React.StrictMode>
 	</Router>
 );
